a99975deb413eed002829f5483f82a8997485978
Kodiak Firesmith <kfiresmith@gmail.com>
2016-07-21 22:12:10 -0400

    Rename to make prog name uniform.

--- dnsdynamic-updater.py
+++ dnsdynamic-updater.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+#
+# A simple python script to keep DnsDynamic.net entries fresh.
+#
+# Kodiak Firesmith <kfiresmith@gmail.com>
+#  Released to the world without license, do what you will.
+# 
+#  Bugs / To-do:  
+#    - Write the state back to the conf file
+#    - Allow for many subdomains rather than just the 1
 
 import os
 import requests
 import configparser
-
-
-# in-script variables
-myipobj = requests.get(ip_provider)
-my_ip = myipobj.text
-update_required = False
-
-
+"""
+Read in config file (/etc/dnsdynamic-updater/dnsdynamic-updater.conf), check current external IP, and if necessary, update
+ the current one.
+"""
 os.chdir("/")
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'etc', 'dnsdynamic-updater', 'dnsdynamic-updater.conf'))
 
-#configparser bits here
 dyn_account = config['main']['dyn_account']
 dyn_passwd = config['main']['dyn_passwd']
 dyn_hostname = config['main']['dyn_hostname']
@@ -24,9 +28,10 @@ api_update_string = config['main']['api_update_string']
 debug_enabled = config['main'].getboolean('debug_enabled')
 last_known_ip = config['state']['last_known_ip']
 
-
 myipobj = requests.get(ip_provider)
 my_ip = myipobj.text
+
+update_required = False
 
 def check_update():
     global update_required
@@ -39,9 +44,6 @@ def update_ddns():
     if debug_enabled:
         print(post_string)
     
-
-    
 check_update()
 if update_required:
     update_ddns()
-
