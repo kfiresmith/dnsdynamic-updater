a56485ff00d474217edfec1bcdc57d3ac0e9f23f
Kodiak Firesmith <kfiresmith@gmail.com>
2016-07-23 15:03:43 -0400

    Some initial error checking work.

--- dnsdynamic-updater.py
+++ dnsdynamic-updater.py
@@ -9,15 +9,24 @@
 #    - Write the state back to the conf file
 #    - Allow for many subdomains rather than just the 1
 
-import os
-import requests
 import configparser
+import os
+import os.path
+import requests
+import socket
+import sys
 """
 Read in config file (/etc/dnsdynamic-updater/dnsdynamic-updater.conf), check current external IP, and if necessary, update
  the current one.
 """
 os.chdir("/")
 config = configparser.ConfigParser()
+conf_abs_path = "/etc/dnsdynamic-updater/dnsdynamic-updater.conf"
+
+if os.path.exists(conf_abs_path) is False:
+    print("dnsdynamic-updater.conf file appears to be missing.")
+    sys.exit(2)
+
 config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'etc', 'dnsdynamic-updater', 'dnsdynamic-updater.conf'))
 
 dyn_account = config['main']['dyn_account']
@@ -30,13 +39,15 @@ last_known_ip = config['state']['last_known_ip']
 
 myipobj = requests.get(ip_provider)
 my_ip = myipobj.text
+my_record = socket.gethostbyname(dyn_hostname)
 
-update_required = False
 
 def check_update():
     global update_required
-    if my_ip != last_known_ip:
+    if my_ip != my_record:
         update_required = True
+        if debug_enabled:
+            print('Update is required; our current IP is ' + str(my_ip) + ', and our record currently lists ' + str(my_record))
 
 def update_ddns():
     post_string = 'https://' + dyn_account + ':' + dyn_passwd + api_update_string + dyn_hostname + '&myip=' + my_ip
@@ -44,6 +55,10 @@ def update_ddns():
     if debug_enabled:
         print(post_string)
     
+update_required = False
+
 check_update()
 if update_required:
     update_ddns()
+
+
